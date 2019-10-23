37c5c62bb1f9efba7dd6f2f386a819fdf71ce1e4
Richa Gupta <richag456@gmail.com>
2019-10-23 13:45:50 -0400

    tried changing the smtp protocols to work correctly

--- signup.php
+++ signup.php
@@ -45,14 +45,13 @@ pg_close($db_connection);
 
 if($account_created){
 	$mail = new PHPMailer(TRUE);
-      $mail->isSMTP();
    
 	  $mail->isSMTP();                                            // Send using SMTP
 	  $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
 	  $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
 	  $mail->Username   = 'ecomm.extraterrestrial@example.com';                     // SMTP username
 	  $mail->Password   = 'AlfWeaver2019';                               // SMTP password
-	  $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+      $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 	  $mail->Port       = 587;                                    // TCP port to connect to
   
    
