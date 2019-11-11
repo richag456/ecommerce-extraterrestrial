a354eaa9ef94246e2acef68af925b535fb75e73e
JustinGalante1 <jn2wf@virginia.edu>
2019-11-10 20:12:22 -0500

    Fixed all navs to login.php

--- contact.php
+++ contact.php
@@ -78,7 +78,7 @@ if( isset($_POST['submit']) ){
                         </li>
                         <li><a href="contact.php">Contact Us</a></li>
                         <li><a href="products.html">Products</a></li>
-                        <li><a href="login.html" class="button primary">Login</a></li>
+                        <li><a href="login.php" class="button primary">Login</a></li>
                     </ul>
                 </nav>
             </header>
