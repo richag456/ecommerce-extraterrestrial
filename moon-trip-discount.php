091fd15594521bb8db0cd310cf09cb1fdd83b47b
JustinGalante1 <jn2wf@virginia.edu>
2019-11-11 19:27:01 -0500

    Final Promocode stuff

--- moon-trip-discount.php
+++ moon-trip-discount.php
@@ -12,11 +12,15 @@ if(!$_SESSION['isLogged']){
 	header('Location: login.php');
 }
 
+if(!$_SESSION['discountMoon']){
+	header('Location: moon-trip.php');
+}
+
 if( isset($_POST['SubmitCode']) ){
 	$user_input = $_POST['code'];
 	if(strcmp($user_input, "YEET") == 0){
-		$_SESSION['discountMars'] = true;
-		header('Location: mars-trip-discount.php');
+		$_SESSION['discountMoon'] = true;
+		header('Location: moon-trip-discount.php');
 	}
 }
 // Close connection
@@ -66,29 +70,29 @@ pg_close($db_connection);
 							<div class="row">
 								<div class="col-4 col-12-xsmall">
 									<figure>
-										<center><img src="images/mars.jpg" width="400" height="400" alt="Image" class="img-fluid"></center>
+										<center><img src="images/moon.jpg" width="400" height="400" alt="Image" class="img-fluid"></center>
 									</figure>
 									<a href="products.html" class="button primary fit">Return to Products</a>
 								</div>
 								<div class="col-6 col-12-xsmall">
 									<div class="px-4">
-											<h3><a href="#">Mars Venture</a></h3>
+											<h3><a href="#">Round-Trip Moon Excursion</a></h3>
 											<ul class="alt">
 												<li>
-														Visit Mars today! Experience the mysteries and magic of Earth's most friendly neighbor planet.
+													Walk in the footsteps of Armstrong and Aldrin! Be among the first to experience the Earth's right hand man.
 													<h4>Details:</h4>
 													<ul>
 														<li>Includes round-trip space travel at a great price.</li>
 														<li>Does NOT include food or lodging.</li>
 													</ul>
 												</li>
-												<li>0.010 BTC</li>
+												<li>0.004 BTC</li>
 												<div class = "row">
 													<div class = "column">
 														<input type="text" name = "code" value = "Enter Promo Code">
 													</div>
 													<div class = "column" style="padding:10px">
-														<form action = "mars-trip.php" method = "POST">
+														<form action = "moon-trip.php" method = "POST">
 															<input type="hidden" name="form_submitted" value="1" />
 															<input type="submit" name="SubmitCode" value="Apply Code">
 														</form>
@@ -99,9 +103,9 @@ pg_close($db_connection);
 														<form action="https://test.bitpay.com/checkout" method="post">
 															<input type="hidden" name="action" value="checkout" />
 															<input type="hidden" name="posData" value="" />
-															<input type="hidden" name="data" value="J1lYV+byihsEl55U2HGD2t0AP6dLRKTnE2SQCmP3OVK8CT75S8U0hvGLPzaQUYpNsiyoZ7spHBM1WgtaQuRk2gbRAV6y3igSv3vtUBtrL2GdS8IUarTzlv7wuTlj4cWnIruvdE+WmOV8NQ7kD9ySRQ==" />
+															<input type="hidden" name="data" value="J1lYV+byihsEl55U2HGD2t0AP6dLRKTnE2SQCmP3OVKvGYkeEFA6M7ecebNelCiOqFX87YPjtmCSZKso+ikgDbG0PCoanUxQezeez5gNMCCDLXH+iH1EM1TzVqBn0i0SF+CVSNfwE9dZz+/OXx1iMzszPkDOU0qgWBlxp+X910M=" />
 															<input type="image" src="https://test.bitpay.com/cdn/en_US/bp-btn-pay-currencies.svg" name="submit" style="width: 210px" alt="BitPay, the easy way to pay with bitcoins.">
-														</form>	
+														</form>
 													</div>
 													<div class = "column" style="padding:10px">
 														<iframe frameBorder="0" scrolling="no" allowtransparency="0" src="https://bitcoinaverage.com/en/widgets?widgetType=conversion&bgcolor=#FFFFFF&bwidth=1&bcolor=#CCCCCC&cstyle=round&fsize=16px&ffamily=arial&fcolor=#000000&bgTransparent=solid&chartStyle=none&lastUpdateTime=none&currency0=USD&total=1" style="width:250px; height:275px; overflow:hidden; background-color:transparent !important;"></iframe>
@@ -112,8 +116,6 @@ pg_close($db_connection);
 								</div>
 							</div>
 						</section>
-
-						
 					</div>
 				</div>
 
